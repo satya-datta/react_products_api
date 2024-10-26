a81ff9f14c0912c6cadefbb530fbb2bf402913e1
Satya <satyadattakallepalli@gmail.com>
2024-10-26 17:52:21 +0530

    changes  in  app.tsx and removing ui elements

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default {
+  server: {
+    port: process.env.PORT || 5173, // Use Render's PORT or fallback to 5173 for local development
+    host: '0.0.0.0', // Ensure the server is accessible from all network interfaces
+  },
+};
